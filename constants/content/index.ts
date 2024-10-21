5141375a121112745d9bf8827d22b97fdd3773b4
Cristhian Valadez <cristian90.jesus@gmail.com>
2024-10-20 23:29:06 -0600

    fix(vercel): fix linked items

--- constants/content/index.ts
+++ constants/content/index.ts
@@ -2,7 +2,6 @@ import { LINKS } from "./links";
 import { profile } from "./profile";
 import { jobExperiences } from "./jobExperiences";
 import { education } from "./education";
-import { portfolioItems } from "./portfolioItems";
 import { socialLinks } from "./socialLinks";
 import { skills } from "./skills";
 
@@ -10,7 +9,6 @@ const CONTENT = {
   ...profile,
   jobExperiences,
   education,
-  portfolioItems,
   socialLinks,
   skills,
 };
