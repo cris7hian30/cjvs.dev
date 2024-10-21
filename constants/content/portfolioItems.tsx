fcd171901e18cca5d82c1eb52f94998d30e19b28
Cristhian Valadez <cristian90.jesus@gmail.com>
2024-10-20 23:35:26 -0600

    chore(build): fix lint errors

--- constants/content/portfolioItems.tsx
+++ constants/content/portfolioItems.tsx
@@ -9,6 +9,6 @@ export const portfolioItems: PortfolioItemProps[] = [
     description:
       "Front End, Animations and Blockchain (Smart contracts) interaction. Built while working with Yuga Labs.",
     link: "https://renga.art/",
-    stack: [sk.jsts, sk.react, sk.next],
+    stack: [sk.framerMotion, sk.react, sk.next],
   },
 ];
