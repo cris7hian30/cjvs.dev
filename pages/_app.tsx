a479f4af53906fa10a3ca4cd634e9561297cbe4a
Cristhian Valadez <cristian90.jesus@gmail.com>
2024-10-22 09:14:50 -0600

    chore(grid): update grid

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,13 +17,13 @@ const App = ({ Component, pageProps }: AppProps) => {
     <AnimatePresence mode="wait">
       <motion.div key={router.pathname}>
         <AnimatedGridPattern
-          numSquares={35}
-          maxOpacity={0.4}
+          numSquares={50}
+          maxOpacity={0.1}
           duration={5}
           repeatDelay={1}
           className={cn(
             "[mask-image:radial-gradient(800px_circle_at_center,white,transparent)]",
-            "fixed inset-0 h-full w-full -z-10 flex items-center justify-center"
+            "fixed inset-0 h-full w-full -z-10 flex items-center justify-center backdrop-blur-lg"
           )}
         />
         <Header />
